c2f34fdb83deafbc48652a8e41b09d15a63cb4b6
Gelila Mamo <gelilambiresaw@gmail.com>
2023-06-03 11:46:39 +0300

    Counter-card update

--- src/portal/Home/Components/counter-card.tsx
+++ src/portal/Home/Components/counter-card.tsx
@@ -1,31 +1,88 @@
 import React from 'react';
-
+import { BookTwoTone,  CheckCircleTwoTone,  CloseCircleTwoTone, InfoCircleTwoTone} from '@ant-design/icons';
+import  { useEffect, useState } from "react";
+import axios from "axios";
+import { baseUrl } from "../../../shared/config";
 export interface CounterCardHeaderProps {
   header: any;
 }
 
-function CounterCard(props: CounterCardHeaderProps) {
+
+  const Headers = [
+    {
+      text: 'Total License',
+      bg: 'bg-blue-100',
+      color: '#0066FF',
+      icon: <BookTwoTone  twoToneColor="#0066FF"style={{fontSize: '25px',}} />,
+      count: 125
+    },
+    {
+      text: 'Pending Licenses',
+      bg: 'bg-yellow-100',
+      color: '#F8B219',
+      icon: <InfoCircleTwoTone twoToneColor="#F8B219"  style={{fontSize: '25px',}}/>,
+      count: 53,
+    },
+    {
+      text: 'Approved Licenses',
+      bg: 'bg-green-100',
+      color: '#52c41a',
+      icon: <CheckCircleTwoTone twoToneColor="#52c41a" style={{fontSize: '25px',}} />,
+      count: 32,
+    },
+    {
+      text: 'Rejected Licenses',
+      bg: 'bg-red-100',
+      color: '#FF0000',
+      icon: <CloseCircleTwoTone twoToneColor="#FF0000" style={{fontSize: '25px',}}/>,
+      count: 12,
+    },
+  ];
+function CounterCard() {
+  // const [licenses , setLicenses] = useState([]);
+
+  // useEffect(() => {
+  //   fetchLicenses();
+  // }, []);
+
+  // const fetchLicenses = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `${baseUrl}user/get-user/`
+  //     );
+  //     setLicenses(response.data);
+  //   } catch (error) {
+  //     console.error("Error fetching licenses:", error);
+  //   }
+  // };
+  
   return (
+    <div className="flex flex-wrap ">
+    {Headers.map((header) => (
     <div className=" w-full sm:w-1/4">
-      <div className="bg-white p-5 rounded-2xl shadow-lg mr-4">
+
+       <div className="bg-white p-5 rounded-2xl shadow-lg mr-4">
         <div className="flex gap-3">
           <div className= " p-5 ">
-            {props.header.icon}
+            {header.icon}
           </div>
 
           <div className="flex flex-col justify-between">
             <div className="font-bold text-xl " >
-              {props.header.text}
+              {header.text}
             </div>
             <span
               className=" text-blue-500 text-lg font-bold"
-              style={{ color: props.header.color }}
+              style={{ color: header.color }}
             >
-              {props.header.count}
+              {header.count.toString()}
             </span>
           </div>
         </div>
       </div>
+    </div>
+                ))}
+
     </div>
   );
 }
--- src/portal/Home/home-page.tsx
+++ src/portal/Home/home-page.tsx
@@ -1,49 +1,19 @@
-import { BookTwoTone,  CheckCircleTwoTone,  CloseCircleTwoTone, InfoCircleTwoTone} from '@ant-design/icons';
 import { useNavigate } from "react-router-dom";
 import CounterCard from "./Components/counter-card";
 import TableCard from './Components/table-card';
 import ProgressChartCard from './Components/progress-chart-card';
 
+
+
 export default function HomePage(){
   const router=useNavigate();
-  const Headers = [
-    {
-      text: 'Total License',
-      bg: 'bg-blue-100',
-      color: '#0066FF',
-      icon: <BookTwoTone  twoToneColor="#0066FF"style={{fontSize: '25px',}} />,
-      count: 108,
-    },
-    {
-      text: 'Pending Licenses',
-      bg: 'bg-yellow-100',
-      color: '#F8B219',
-      icon: <InfoCircleTwoTone twoToneColor="#F8B219"  style={{fontSize: '25px',}}/>,
-      count: 53,
-    },
-    {
-      text: 'Approved Licenses',
-      bg: 'bg-green-100',
-      color: '#52c41a',
-      icon: <CheckCircleTwoTone twoToneColor="#52c41a" style={{fontSize: '25px',}} />,
-      count: 32,
-    },
-    {
-      text: 'Rejected Licenses',
-      bg: 'bg-red-100',
-      color: '#FF0000',
-      icon: <CloseCircleTwoTone twoToneColor="#FF0000" style={{fontSize: '25px',}}/>,
-      count: 12,
-    },
-  ];
+  
   
   return(
     <>
     <div className="p-4 gap-4 flex flex-col gap-4">
-        <div className="flex flex-wrap ">
-          {Headers.map((header, h) => (
-            <CounterCard header={header} key={h} />
-          ))}
+        <div >
+            <CounterCard/>
         </div>
         <div style={{ display: 'flex' }} className="flex gap-4">
           <div style={{ flex: '8' }}>
