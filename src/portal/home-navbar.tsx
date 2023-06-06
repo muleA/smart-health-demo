53232f99829720e885bfe2b67cd76756ebd97c54
Yohannes Berhanu <yohannesberhaanu@gmail.com>
2023-06-06 15:07:33 +0300

    removed unused variables

--- src/portal/home-navbar.tsx
+++ src/portal/home-navbar.tsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react';
-import { Layout, Menu, Dropdown } from 'antd';
-import { UserOutlined, CaretDownOutlined, LogoutOutlined, MenuFoldOutlined, MenuUnfoldOutlined, HomeOutlined, SettingOutlined, EditOutlined, FileAddOutlined, FolderOutlined, BellOutlined, UnorderedListOutlined } from '@ant-design/icons';
-import { Link, useNavigate, Routes, Route } from 'react-router-dom';
-import MyApplications from './my-applications';
-import MyLicenses from './my-archives';
-import ProfileWrapper from './profile-wrapper';
-import { useAuth } from '../shared/auth/use-auth';
-import HomePage from './Home/home-page';
-import Banner from '../pages/banner';
-import StepperComponent from './application-form';
-import ApplicationDetail  from './application-detail';
-import Archives from './my-archives';
-import Notification from './notification';
+import React, { useState } from "react";
+import { Layout, Menu, Dropdown } from "antd";
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  LogoutOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  HomeOutlined,
+  SettingOutlined,
+  EditOutlined,
+  FileAddOutlined,
+  FolderOutlined,
+  BellOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
+import { Link, useNavigate, Routes, Route } from "react-router-dom";
+import MyApplications from "./my-applications";
+import MyLicenses from "./my-archives";
+import ProfileWrapper from "./profile-wrapper";
+import { useAuth } from "../shared/auth/use-auth";
+import HomePage from "./Home/home-page";
+import Banner from "../pages/banner";
+import StepperComponent from "./application-form";
+import ApplicationDetail from "./application-detail";
+import Archives from "./my-archives";
+import Notification from "./notification";
 
 const { Header, Content, Sider } = Layout;
 
@@ -22,17 +35,17 @@ const PortalNavigation = () => {
   };
   const { logOut } = useAuth();
   const router = useNavigate();
-  const [selectedMenu, setSelectedMenu] = useState('1'); // Track the selected menu item
+  const [selectedMenu, setSelectedMenu] = useState("1"); // Track the selected menu item
 
   const handleLogout = () => {
     logOut();
-    router('/');
+    router("/");
   };
 
-  const handleMenuSelect = ({ key }:any) => {
+  const handleMenuSelect = ({ key }: any) => {
     setSelectedMenu(key);
   };
-const {session}=useAuth()
+  const { session } = useAuth();
   const accountMenu = (
     <Menu className="text-primary">
       <Menu.Item key="1" onClick={handleLogout} icon={<LogoutOutlined />}>
@@ -46,15 +59,15 @@ const {session}=useAuth()
 
   const renderContent = () => {
     switch (selectedMenu) {
-      case '1':
+      case "1":
         return <ProfileWrapper />;
-      case '2':
+      case "2":
         return <MyApplications />;
-      case '3':
+      case "3":
         return <MyLicenses />;
       /* case '4':
         return <Notification />; */
-      case '5':
+      case "5":
         return <Notification />;
       default:
         return null;
@@ -62,7 +75,7 @@ const {session}=useAuth()
   };
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
+    <Layout style={{ minHeight: "100vh" }}>
       <Sider
         width={200}
         theme="light"
@@ -75,25 +88,25 @@ const {session}=useAuth()
           mode="vertical"
           className="mt-2"
           theme="light"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={["1"]}
           onSelect={handleMenuSelect}
         >
-           <Menu.Item key="1" icon={<HomeOutlined />}>
+          <Menu.Item key="1" icon={<HomeOutlined />}>
             <Link to="/home">Home</Link>
           </Menu.Item>
           <Menu.Item key="2" icon={<UserOutlined />}>
             <Link to="/my-profile">My Profile</Link>
           </Menu.Item>
           <Menu.Item key="3" icon={<UnorderedListOutlined />}>
-  <Link to="/my-applications">My Applications</Link>
-</Menu.Item>
-<Menu.Item key="8" icon={<FileAddOutlined />}>
-  <Link to="/new-application">New Applications</Link>
-</Menu.Item>
+            <Link to="/my-applications">My Applications</Link>
+          </Menu.Item>
+          <Menu.Item key="8" icon={<FileAddOutlined />}>
+            <Link to="/new-application">New Applications</Link>
+          </Menu.Item>
 
-<Menu.Item key="5" icon={<FolderOutlined />}>
-  <Link to="/my-archives">My Archives</Link>
-</Menu.Item>
+          <Menu.Item key="5" icon={<FolderOutlined />}>
+            <Link to="/my-archives">My Archives</Link>
+          </Menu.Item>
 
           <Menu.Item key="6" icon={<BellOutlined />}>
             <Link to="/settings">Notifications</Link>
@@ -104,7 +117,7 @@ const {session}=useAuth()
         </Menu>
       </Sider>
       <Layout>
-      {/*   <Header
+        {/*   <Header
           className="bg-gray-200"
           style={{
             display: 'flex',
@@ -137,15 +150,15 @@ const {session}=useAuth()
           </div>
         </Header> */}
 
-        <Content style={{ margin: '16px' }}>
+        <Content style={{ margin: "16px" }}>
           <Routes>
-            <Route path="/home" element ={<HomePage/>} />
-            <Route  path='/banner' element={<Banner/>}></Route>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/banner" element={<Banner />}></Route>
             <Route path="/my-profile" element={<ProfileWrapper />} />
             <Route path="/my-applications" element={<MyApplications />} />
             <Route path="/new-application" element={<StepperComponent />} />
             <Route path="/my-archives" element={<Archives />} />
-            <Route path="my-applications/:id" element={<ApplicationDetail/>}/>
+            <Route path="my-applications/:id" element={<ApplicationDetail />} />
             {/* Add more routes here */}
           </Routes>
         </Content>
